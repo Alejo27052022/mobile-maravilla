8b8c4a20d16c63195b13c95b9947db3ddbb3fe06
Alejo27052022 <stalinalejandro19@outlook.es>
2025-03-06 17:47:59 -0500

    Finalizacion del Proyecto

--- app/src/main/java/com/example/myapplication/Perfil.kt
+++ app/src/main/java/com/example/myapplication/Perfil.kt
@@ -2,21 +2,34 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.adaptadores.MisPedidosAdapter
+import com.example.myapplication.data.Clientes
+import com.example.myapplication.service.RetrofitInstance
+import com.example.myapplication.service.RetrofitService
 import com.example.myapplication.viewmodel.MisPedidosViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Perfil : ComponentActivity() {
 
+    private lateinit var retrofitService: RetrofitService
     private lateinit var misPedidosViewModel: MisPedidosViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pantalla_profile)
+
+        retrofitService = RetrofitInstance.getRetrofitAuth(this)
 
         val iconhome : ImageView = findViewById(R.id.icon_home)
         iconhome.setOnClickListener {
@@ -44,5 +57,31 @@ class Perfil : ComponentActivity() {
         }
 
         misPedidosViewModel.obtenerMisPedidos()
+        fetchUserProfile()
+    }
+
+    private fun fetchUserProfile() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val cliente: Clientes = retrofitService.getUserProfile()
+                withContext(Dispatchers.Main) {
+                    updateUI(cliente)
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Log.e("PerfilActivity", "Error fetching user profile", e)
+                    Toast.makeText(this@Perfil, "Error fetching user profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun updateUI(cliente: Clientes) {
+        findViewById<TextView>(R.id.txt_nombre_apellido).text = "${cliente.first_name} ${cliente.last_name}"
+        findViewById<TextView>(R.id.txt_username).text = cliente.username
+        findViewById<TextView>(R.id.txt_email).text = cliente.email
+        findViewById<TextView>(R.id.txt_telefono).text = cliente.profile.telefono
+        findViewById<TextView>(R.id.txt_address).text = cliente.profile.direccion
+        // Puedes agregar más TextViews si necesitas mostrar más información
     }
 }
