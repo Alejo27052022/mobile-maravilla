dd67ec023dd92cbed20b872c0066b132e57e6309
Stalin1206 <stalin.consuegra@iroute.com.ec>
2025-11-11 16:22:41 -0500

    feat: Change Array Images

--- app/src/main/java/com/example/myapplication/adaptadores/CategoriaAdapter.kt
+++ app/src/main/java/com/example/myapplication/adaptadores/CategoriaAdapter.kt
@@ -3,8 +3,10 @@ package com.example.myapplication.adaptadores
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.data.Categoria
 
@@ -26,16 +28,21 @@ class CategoriaAdapter(
     override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
         val categoria = categorias[position]
         holder.nombreCategoria.text = categoria.nombre
-
         // Capturar el clic en la categoría
         holder.itemView.setOnClickListener {
             listener.onCategoriaClick(categoria)
         }
+        Glide.with(holder.imageCategoria)
+            .load(categoria.imgPlato)
+            .placeholder(R.drawable.icon_platofuerte)
+            .error(R.drawable.error_icon)
+            .into(holder.imageCategoria)
     }
 
     override fun getItemCount(): Int = categorias.size
 
     inner class CategoriaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageCategoria: ImageView = itemView.findViewById(R.id.image_categoria)
         val nombreCategoria: TextView = itemView.findViewById(R.id.nombre_categoriaData)
     }
 }
--- app/src/main/java/com/example/myapplication/data/categoria.kt
+++ app/src/main/java/com/example/myapplication/data/categoria.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication.data
+import com.google.gson.annotations.SerializedName
 
 data class Categoria(
     val id: Int,
-    val nombre: String
+    val nombre: String,
+    @SerializedName("img_categoria") val imgPlato: String
 )
